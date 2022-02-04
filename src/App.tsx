88fd23468fa27b7b371ca47d2deae0fa00243ba7
brabyr <brabyrne@gmail.com>
2022-02-04 23:55:10 +0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const host = process.env.API_HOST;
+  console.log('HOST ==>', host);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit
+          <code>src/App.tsx</code>
+          and save to reload.
         </p>
         <a
           className="App-link"
